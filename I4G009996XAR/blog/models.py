39150702f9c63edeb0d127f8eb240b11c2e25a7d
George Stephen <stephengeorge022@gmail.com>
2022-06-18 20:41:36 -0700

    first commit

---
+++ I4G009996XAR/blog/models.py
@@ -0,0 +1,18 @@
+from turtle import title
+from django.db import models
+from datetime import datetime
+from distutils.text_file import TextFile
+from lib2to3.refactor import get_all_fix_names
+from typing import Text
+from django.db import models
+from django.contrib.auth import get_user_model
+import mmap
+
+# Create your models here.
+
+class post(models.Model):
+    Title: models.CharField(max_length=200)
+    Text: models.TextField(max_length=500)
+    Author: models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_date: models.DateTimeField(default=datetime.today)
+    Published_date: models.DateTimeField(default=datetime.today)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zuri-django-model
+django model zuri intenship task
